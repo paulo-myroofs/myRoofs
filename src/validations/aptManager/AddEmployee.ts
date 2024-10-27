9bab405bf8b10bdfb9d25c4f1360ae60d55f1e48
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2024-10-27 18:35:04 -0300

    fix(employeeForm): added address fields to the zod object

--- src/validations/aptManager/AddEmployee.ts
+++ src/validations/aptManager/AddEmployee.ts
@@ -5,15 +5,22 @@ import name from "@/common/validation/name";
 import numberWithAux from "@/common/validation/numberWithAux";
 import phone from "@/common/validation/phone";
 
+const address = z.object({
+  cep: z.string({ required_error: "CEP é obrigatório" }),
+  state: z.string({ required_error: "Estado é obrigatório" }),
+  city: z.string({ required_error: "Cidade é obrigatória" }),
+  neighborhood: z.string({ required_error: "Bairro é obrigatório" }),
+  address: z.string({ required_error: "Endereço é obrigatório" }),
+  number: z.string({ required_error: "Número é obrigatório" })
+});
+
 export default z.object({
   name,
   email,
   occupation: z
     .string({ required_error: "Insira um valor" })
     .min(1, "Insira um valor"),
-  address: z
-    .string({ required_error: "Insira um valor" })
-    .min(1, "Insira um valor"),
+  address,
   phone,
   cpf: numberWithAux
 });
