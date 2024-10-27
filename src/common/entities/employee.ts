2fa0184ef54b469271f143d958d8558efea07921
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2024-10-27 18:45:28 -0300

    fix(employeeForm): added address interface to employee entity

--- src/common/entities/employee.ts
+++ src/common/entities/employee.ts
@@ -1,5 +1,14 @@
 import { Timestamp } from "./timestamp";
 
+export interface EmployeeAddress {
+  cep: string;
+  state: string;
+  city: string;
+  neighborhood: string;
+  address: string;
+  number: string;
+}
+
 export interface EmployeeEntity {
   id: string;
   image: string | null;
@@ -8,7 +17,7 @@ export interface EmployeeEntity {
   phone?: string;
   cpf: string;
   occupation: string;
-  address: string;
+  address: EmployeeAddress;
   createdAt: Timestamp;
   updatedAt: Timestamp;
   condominiumCode: string;
