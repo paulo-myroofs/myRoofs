b9bf37e77654fc2a9a9b7aea48a89c418416e3c6
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2024-10-31 00:23:47 -0300

    feature(main): add smooth transation on new condo page

--- src/app/(authenticated)/sindico/novo-condominio/page.tsx
+++ src/app/(authenticated)/sindico/novo-condominio/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect } from "react";
+
 import { AptManagerEntity } from "@/common/entities/aptManager";
 import TitleAtom from "@/components/atoms/TitleAtom/TitleAtom";
 import AddEditCondoForm from "@/components/organism/AddEditCondoForm/AddEditCondoForm";
@@ -9,6 +11,16 @@ import useAuth from "@/hooks/useAuth";
 const NewCondo = () => {
   const { userUid } = useAuth();
   const { data: aptManager } = useProfile<AptManagerEntity>(userUid);
+
+  // Scroll suave para o topo da página
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }, 0);
+  }, []);
 
   return (
     <section className="mx-auto block w-11/12 max-w-[885px] pb-[40px]">
