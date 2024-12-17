24f2e2ac6f117cc843971df1acd6a0e2902c1e45
Ênio Henrique <62400466+ribeirowski@users.noreply.github.com>
2024-12-17 12:24:03 -0300

    feat(commonAreaIcon): added new common area options

--- src/components/atoms/Icons/Gym.tsx
+++ src/components/atoms/Icons/Gym.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Gym = ({ color = "#00DC52" }: { color?: string }) => {
   return (
     <svg
-      width="24"
-      height="24"
+      width="28"
+      height="28"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- src/components/molecules/CommonAreaInputs/components/IconsSelector/IconsSelector.tsx
+++ src/components/molecules/CommonAreaInputs/components/IconsSelector/IconsSelector.tsx
@@ -9,16 +9,22 @@ const IconsSelector = ({ activeIcon, setActiveIcon }: IconsSelectorProps) => {
   return (
     <div className="flex flex-col gap-1">
       <Label>Ícone</Label>
-      <div className="flex gap-2">
+      <div className="align-center flex flex-wrap gap-2">
         {commonAreaIcons.map((data) => (
-          <button
-            type="button"
-            className={`flex h-[50px] w-[50px] items-center justify-center overflow-hidden rounded-full border border-verde-escuro  transition-all ${activeIcon === data.type && "scale-110"} ${activeIcon && activeIcon !== data.type && "opacity-50"} `}
-            key={data.type}
-            onClick={() => setActiveIcon(data.type)}
-          >
-            {data.icon()}
-          </button>
+          <div className="align-center flex flex-col" key={data.type}>
+            <button
+              type="button"
+              className={`flex h-[90px] w-[90px] items-center justify-center overflow-hidden rounded-full border border-verde-escuro  transition-all ${activeIcon === data.type && "scale-110"} ${activeIcon && activeIcon !== data.type && "opacity-50"} `}
+              onClick={() => setActiveIcon(data.type)}
+            >
+              {data.icon()}
+            </button>
+            <p
+              className={`mb-2 text-center text-xs ${activeIcon === data.type && "mb-0 mt-2 scale-110"} ${activeIcon && activeIcon !== data.type && "opacity-50"} transition-all`}
+            >
+              {data.name}
+            </p>
+          </div>
         ))}
       </div>
     </div>
