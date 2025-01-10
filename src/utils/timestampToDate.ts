f2f5dd65dfa31fb5c89f215cc35dbd837aa32fdf
hlpaa <hlpaa@cin.ufpe.br>
2025-01-09 22:23:41 -0300

    throw error added

--- src/utils/timestampToDate.ts
+++ src/utils/timestampToDate.ts
@@ -1,6 +1,12 @@
-import { Timestamp } from "firebase/firestore";
+import { Timestamp } from "@/common/entities/timestamp";
 
 export const timestampToDate = (data: Timestamp): Date => {
+
+  
+  if (!data || typeof data.seconds !== "number" || typeof data.nanoseconds !== "number") {
+    throw new Error("Invalid Timestamp object");
+  }
+
   const milliseconds = data.seconds * 1000 + data.nanoseconds / 1000000;
   return new Date(milliseconds);
 };
