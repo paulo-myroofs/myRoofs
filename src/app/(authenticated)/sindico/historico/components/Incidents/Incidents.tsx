07cc85a1fb4b17e4668de5a17c98539b70f71493
Heitor <heitorfelipe37@gmail.com>
2025-02-17 14:30:29 -0300

    bugfix([fix/occurrencesTable]): adding print button and adding mocked condoName to table's header

--- src/app/(authenticated)/sindico/historico/components/Incidents/Incidents.tsx
+++ src/app/(authenticated)/sindico/historico/components/Incidents/Incidents.tsx
@@ -5,6 +5,8 @@ import { useState } from "react";
 
 import { twMerge } from "tailwind-merge";
 
+import Image from "next/image";
+
 import { OccurrenceEntity } from "@/common/entities/occurrences";
 import { DataPaginatedTable } from "@/components/atoms/DataTablePaginated/DataTablePaginated";
 import Input from "@/components/atoms/Input/input";
@@ -13,6 +15,7 @@ import { storageGet } from "@/store/services/storage";
 
 import { columns } from "./columns";
 import { OccurrenceColumnData } from "./types";
+import Button from "@/components/atoms/Button/button";
 
 const boxStyle = "border border-black rounded-[8px]";
 
@@ -48,20 +51,26 @@ const IncidentsTable = () => {
   return (
     <section className="space-y-4">
       <div className={twMerge(boxStyle)}>
-        <div className="flex flex-col items-center justify-between gap-y-2 px-8 py-4 sm:flex-row">
+        <div className="flex items-center justify-between gap-y-2 px-8 py-4 sm:flex-row">
           <h1 className="text-[18px] font-bold sm:text-[24px]">
-            <span className="hidden sm:inline">Histórico de ocorrências</span>
+            <span className="hidden sm:inline">Histórico de ocorrências - CondoName</span>
             <span className="inline sm:hidden">Ocorrências</span>
           </h1>
 
-          <Input
-            className={
-              "w-full border border-[#DEE2E6] bg-[#F8F9FA] focus:border-[#DEE2E6] sm:max-w-[400px]"
-            }
-            placeholder="Pesquise um ocorrência pelo título"
-            value={filterValue}
-            onChange={(e) => setFilterValue(e.target.value)}
-          />
+          <div className="flex items-center gap-x-2 w-auto lg:w-[410px] md:w-[410px] ml-2">
+            <Button className="text-2xs items-center gap-x-2 p-2 sm:p-3 min-w-0 w-auto sm:w-36">
+              <Image src="/icons/historic/download_button.svg" width={15} height={15} alt="download Icon"></Image>
+              <span className="hidden sm:flex">Imprimir</span>
+            </Button>
+            <Input
+              className={
+                "w-full border border-[#DEE2E6] bg-[#F8F9FA] focus:border-[#DEE2E6] sm:max-w-[400px]"
+              }
+              placeholder="Pesquise uma ocorrência pelo título"
+              value={filterValue}
+              onChange={(e) => setFilterValue(e.target.value)}
+            />
+          </div>
         </div>
         <span className="block h-[0.5px] w-full bg-black" />
         <div className="mx-auto flex w-11/12 max-w-[1200px] items-center justify-center pb-8 sm:py-8">
