381eb43fb20e17d50b823ccd2fdf5f2533d3c07f
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-02-24 13:29:53 -0300

    bugfix(condo-info): Adjusted Formation's fields

--- src/components/organism/AddEditCondoForm/AddEditCondoForm.tsx
+++ src/components/organism/AddEditCondoForm/AddEditCondoForm.tsx
@@ -449,20 +449,26 @@ const AddEditCondoForm = ({
             <InputField
               className={inputClassName + " disabled:opacity-50"}
               label={"Nome da formação " + (index + 1)}
-              value={internalOrgInputs?.names[index as number]}
+              value={internalOrgInputs?.names[index as number] ?? ""}
               onChange={(e) =>
-                setInternalOrgInputs((prev) => {
-                  if (!prev) return;
-                  return {
-                    ...prev,
-                    names: prev.names.map((item, ind) =>
-                      index === ind ? e.target.value : item
-                    )
-                  };
-                })
+                setInternalOrgInputs((prev) =>
+                  prev
+                    ? {
+                        ...prev,
+                        names: prev.names.map((item, ind) =>
+                          index === ind ? e.target.value : item
+                        )
+                      }
+                    : undefined
+                )
               }
               placeholder="Digite o nome da formação"
-              disabled={isEditing}
+              disabled={
+                isEditing &&
+                condoData?.formationNames?.includes(
+                  internalOrgInputs?.names[index as number]
+                )
+              }
             />
             {!readOnly &&
               index !== 0 &&
@@ -474,13 +480,14 @@ const AddEditCondoForm = ({
                   size={18}
                   className="absolute right-0 top-0 opacity-70 hover:opacity-100"
                   onClick={() =>
-                    setInternalOrgInputs((prev) => {
-                      if (!prev) return;
-                      return {
-                        ...prev,
-                        names: prev?.names.filter((_, ind) => ind !== index)
-                      };
-                    })
+                    setInternalOrgInputs((prev) =>
+                      prev
+                        ? {
+                            ...prev,
+                            names: prev?.names.filter((_, ind) => ind !== index)
+                          }
+                        : undefined
+                    )
                   }
                 />
               )}
