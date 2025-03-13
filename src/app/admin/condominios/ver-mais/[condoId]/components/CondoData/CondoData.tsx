06f740a8bc0f0e6f24b85ca3820aa0f3ba7ca849
Luiza Trigueiro <lulu.study.2005@gmail.com>
2025-03-13 09:54:30 -0300

    bugfix(forms-admin): Added edition to admin's form

--- src/app/admin/condominios/ver-mais/[condoId]/components/CondoData/CondoData.tsx
+++ src/app/admin/condominios/ver-mais/[condoId]/components/CondoData/CondoData.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
+import Button from "@/components/atoms/Button/button";
 import LoadingComponent from "@/components/atoms/Loading/loading";
 import AddEditCondoForm from "@/components/organism/AddEditCondoForm/AddEditCondoForm";
 import useCondo from "@/hooks/queries/condos/useCondo";
 
 const CondoData = ({ condoId }: { condoId: string }) => {
   const { data: condo } = useCondo(condoId);
+  const [isEdit, setIsEdit] = useState(false);
 
   if (!condo) {
     return (
@@ -15,13 +17,24 @@ const CondoData = ({ condoId }: { condoId: string }) => {
     );
   }
   return (
-    <div className="mx-auto max-w-[1200px]">
+    <div className="mx-auto max-w-[1200px] space-y-8">
       <AddEditCondoForm
         aptManagersIds={condo?.aptManagersIds}
         companyId={condo?.companyId}
-        readOnly
+        readOnly={!isEdit}
         condoData={condo}
+        setEditFalse={() => setIsEdit(false)}
       />
+      {!isEdit && (
+        <Button
+          variant="basicBlack"
+          onClick={() => setIsEdit(true)}
+          className="mx-auto"
+        >
+          {" "}
+          Editar{" "}
+        </Button>
+      )}
     </div>
   );
 };
